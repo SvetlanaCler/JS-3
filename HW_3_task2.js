4cf6d22b1a46f850b528f596922c10c6bf0eaad4
SvetlanaCler <clerforlani@hotmail.com>
2024-09-20 03:03:26 +0300

    2 tasks

---
+++ HW_3_task2.js
@@ -0,0 +1,17 @@
+
+const n = 7;
+const a1 = String(n);
+const a2 = String(n) + a1;
+const a3 = String(n) + a2;
+
+console.log(+a1 + +a2 + +a3) //861
+
+
+
+// const n = 1;
+// const a1 = +n;
+// const a2 = +n + a1; 
+// const a3 = +n + a2;
+
+// console.log(Number(a1) + Number(a2) + Number(a3)) //6
+
